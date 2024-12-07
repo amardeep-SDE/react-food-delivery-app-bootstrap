382dc45ded05a05c730aeafa87d59c0c8714aa90
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2024-12-07 22:23:25 +0530

    feat: section2 about, section3 menu and card

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Layout from '../../components/Layouts/Layout'
-import Section1 from './Section1'
 import "../../styles/HomeStyle.css"
+import Section1 from './Section1'
+import Section2 from './Section2'
+import Section3 from './Section3'
 
 function Home() {
   return (
@@ -9,6 +11,10 @@ function Home() {
       <Layout>
      {/* Home Section Hero Banner */}
      <Section1/>
+     {/* Home section about */}
+     <Section2/>
+     {/* Home section Menu */}
+     <Section3/>
       </Layout>
     </>
   )
